e6a66e1915b6cb7a055a901b91b260e4c76a5a8c
849010500 <849010500@qq.com>
2026-05-12 01:20:29 +0800

    v0.1.4: Add strings.json, simplify config_flow

--- custom_components/ikuai_router/config_flow.py
+++ custom_components/ikuai_router/config_flow.py
@@ -1,21 +1,29 @@
-"""
-配置流程模块
-"""
+"""Config flow for iKuai Router."""
 import logging
-from typing import Any, Dict, Optional
 
 import voluptuous as vol
+
 from homeassistant import config_entries
-from homeassistant.core import callback
-from .const import DOMAIN, CONFIG_ENTRY_TITLE, CONF_BASE_URL, CONF_TOKEN, CONF_BINARY_PATH, DEFAULT_BINARY_PATH
+
+from .const import (
+    DOMAIN,
+    CONFIG_ENTRY_TITLE,
+    CONF_BASE_URL,
+    CONF_TOKEN,
+    CONF_BINARY_PATH,
+    DEFAULT_BINARY_PATH,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
-DATA_SCHEMA = vol.Schema({
-            vol.Required(CONF_BASE_URL, default="http://192.168.1.1"): str,
-            vol.Required(CONF_TOKEN): str,
-            vol.Optional(CONF_BINARY_PATH, default=DEFAULT_BINARY_PATH): str,
-        })
+
+DATA_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_BASE_URL, default="http://192.168.1.1"): str,
+        vol.Required(CONF_TOKEN): str,
+        vol.Optional(CONF_BINARY_PATH, default=DEFAULT_BINARY_PATH): str,
+    }
+)
 
 
 class IkuaiConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -24,24 +32,12 @@ class IkuaiConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         if user_input is not None:
-            return self.async_create_entry(title=CONFIG_ENTRY_TITLE, data=user_input)
+            return self.async_create_entry(
+                title=CONFIG_ENTRY_TITLE,
+                data=user_input,
+            )
 
-        return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA)
-
-    @staticmethod
-    @callback
-    def async_get_options_flow(config_entry):
-        return IkuaiOptionsFlowHandler(config_entry)
-
-
-class IkuaiOptionsFlowHandler(config_entries.OptionsFlow):
-
-    async def async_step_init(self, user_input=None):
-        if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
-
-        schema = vol.Schema({
-            vol.Optional(CONF_TOKEN, default=self.config_entry.data.get(CONF_TOKEN, "")): str
-        })
-        
-        return self.async_show_form(step_id="init", data_schema=schema)
+        return self.async_show_form(
+            step_id="user",
+            data_schema=DATA_SCHEMA,
+        )
